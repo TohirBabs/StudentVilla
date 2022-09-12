ff323d4c47d4d7c25a30046fed487fa6b8eb3b3c
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2022-09-12 14:54:41 +0100

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,8 +5,8 @@ import { Button } from "@mui/material";
 
 export const Navbar = () => {
   return (
-    <nav className="sticky top-2 rounded-2xl sm:max-w-5xl max-w-[96vw] z-10 mx-auto bg-white text-black backdrop-filter backdrop-blur-lg bg-opacity-70">
-      <div className="mx-auto px-4 py-2">
+    <nav className="sticky top-2 rounded-full sm:max-w-5xl max-w-[96vw] z-10 mx-auto bg-white text-black backdrop-filter backdrop-blur-lg bg-opacity-70">
+      <div className="mx-auto px-4 py-1">
         <div className="flex items-center justify-between ">
           <span className="text-xl text-gray-900 font-semibold">Villa</span>
           <div className="flex space-x-4 text-gray-900">
